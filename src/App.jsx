8019835af9dadf816f86ecaed84a0ca3c82efd90
eeyzot <ozgehantalay@gmail.com>
2026-02-27 08:50:24 +0300

    Implement Cocktails section with animation.

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import Hero from './components/Hero';
 import Cocktails from './components/Cocktails';
 import About from './components/About';
 import Art from './components/Art';
+import Menu from './components/Menu';
 
 
 //because these GAP plugins aren't automatically active. 
@@ -21,6 +22,7 @@ const App = () => {
       <Cocktails />
       <About />
       <Art />
+      <Menu />
     </main>
   )
 }
